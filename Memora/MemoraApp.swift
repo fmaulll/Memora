132239b630c5163b7c819507007ef3345a4a7a71
maul <maul2821@gmail.com>
2026-08-13 12:42:36 +0700

    Initial Commit

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,32 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by fuckdazeshit on 13/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MemoraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MemoraTests/MemoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraTests.swift
+//  MemoraTests
+//
+//  Created by fuckdazeshit on 13/08/26.
+//
+
+import Testing
+@testable import Memora
+
+struct MemoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
