b8ede749c268f27761cbea18d10fabcc3eae0801
maul <maul2821@gmail.com>
2026-08-15 23:43:00 +0700

    app background become static :(

---
+++ Memora/Views/Shared/AppBackground.swift
@@ -0,0 +1,28 @@
+//
+//  AppBackground.swift
+//  Memora
+//
+//  Created by fuckdazeshit on 14/08/26.
+//
+
+import SwiftUI
+
+struct AppBackground<Content: View>: View {
+    @ViewBuilder let content: Content
+
+    private let background = Color(red: 0.04, green: 0.04, blue: 0.13)
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        ZStack {
+            background.ignoresSafeArea()
+            DecorativeBackground()
+                .ignoresSafeArea()
+
+            content
+        }
+    }
+}
