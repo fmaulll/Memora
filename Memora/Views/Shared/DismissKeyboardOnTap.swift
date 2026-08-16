78ad63cc7d57214265c1a8426f0049226d60710f
maul <maul2821@gmail.com>
2026-08-17 00:14:34 +0700

    dismiss keyboard

---
+++ Memora/Views/Shared/DismissKeyboardOnTap.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension View {
+    /// Dismisses the keyboard when the user taps anywhere else on screen.
+    func dismissKeyboardOnTap() -> some View {
+        simultaneousGesture(
+            TapGesture().onEnded {
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+        )
+    }
+}
