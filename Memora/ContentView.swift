246df4432d54fb022a1878f5dc40be384b8bd22b
maul <maul2821@gmail.com>
2026-08-17 10:32:34 +0700

    splash

--- Memora/ContentView.swift
+++ Memora/ContentView.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingSplash = true
+
     var body: some View {
         NavigationStack {
-            WelcomeView()
+            if isShowingSplash {
+                SplashScreenView {
+                    withAnimation(.easeInOut(duration: 0.4)) {
+                        isShowingSplash = false
+                    }
+                }
+            } else {
+                WelcomeView()
+            }
         }
     }
 }
