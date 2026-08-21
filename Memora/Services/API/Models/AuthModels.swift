4bf51303bef683a59a6c21432d890cae2e71324f
maul <maul2821@gmail.com>
2026-08-21 17:52:33 +0700

    auth models

---
+++ Memora/Services/API/Models/AuthModels.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+struct RegisterRequest: Encodable {
+    let name: String
+    let email: String
+    let password: String
+}
+
+struct LoginRequest: Encodable {
+    let email: String
+    let password: String
+}
+
+struct UserUpdateRequest: Encodable {
+    let name: String?
+    let email: String?
+}
+
+struct TokenResponse: Decodable {
+    let accessToken: String
+    let tokenType: String
+
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+    }
+}
+
+struct UserResponse: Decodable {
+    let id: UUID
+    let name: String
+    let email: String
+    let createdAt: Date
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case createdAt = "created_at"
+    }
+}
